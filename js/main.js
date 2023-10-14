98095a8babb5ced7b9c7c9a03db554e9ab10d6fa
Basem Samir <138936789+BasemSG@users.noreply.github.com>
2023-10-15 00:50:59 +0300

    Delete js/main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const toogoler = document.querySelector("img.toogle");
-const nav = document.querySelector("ul.links");
-
-toogoler.addEventListener("click", function () {
-  nav.classList.toggle("show");
-  console.log("clicked");
-});
